98e2f80460eef1bbad98ac534be839d2b0047ebe
Jeremy Iverson <jiverson002@gmail.com>
2021-01-04 13:03:07 -0600

    chkpt

--- include/vcuda/core.h
+++ include/vcuda/core.h
@@ -5,61 +5,7 @@
 #include <cstddef>
 #include <vector>
 
-#include "vcuda/auto_cast.h"
 #include "vcuda/core/export.h"
-
-/*----------------------------------------------------------------------------*/
-/*! Black magic. */
-/*----------------------------------------------------------------------------*/
-#define _SELECT(_1, _2, _3, _4, NAME, ...) NAME
-#define SELECT(f,...) _SELECT( __VA_ARGS__\
-                             , f ## 4\
-                             , f ## 3\
-                             , f ## 2\
-                             , f ## 1\
-                             , XXX\
-                             )(__VA_ARGS__)
-
-#define VCUDA_GET_ARG4(arg1, ...)\
-  VCUDA_GET_ARG1(arg1) VCUDA_GET_ARG3(__VA_ARGS__)
-#define VCUDA_GET_ARG3(arg1, ...)\
-  VCUDA_GET_ARG1(arg1) VCUDA_GET_ARG2(__VA_ARGS__)
-#define VCUDA_GET_ARG2(arg1, ...)\
-  VCUDA_GET_ARG1(arg1) VCUDA_GET_ARG1(__VA_ARGS__)
-#define VCUDA_GET_ARG1(arg1) arg1 = auto_cast(kernelParams[VCUDA_kp_ctr++]);
-
-#define VCUDA_get_args(...) SELECT(VCUDA_GET_ARG, __VA_ARGS__)
-
-#define VCUDA_defn_make(scope, name) \
-  static void\
-  VCUDA_kernel_ ## name(\
-    size_t VCUDA_kp_ctr,\
-    const dim3 gridDim,\
-    const dim3 blockDim,\
-    const dim3 blockIdx,\
-    const dim3 threadIdx,\
-    void **    kernelParams,\
-    void **    extra\
-  )\
-  {\
-    VCUDA_get_args
-
-#define VCUDA_defn_args(scope, name) VCUDA_defn_make(scope, name)
-#define VCUDA_defn_name(rest)        rest
-#define VCUDA_defn_void              VCUDA_defn_name(
-#define VCUDA_defn_type(rest)        VCUDA_defn_ ## rest)
-#define VCUDA_defn_extern            VCUDA_defn_args(extern, VCUDA_defn_type(
-#define VCUDA_defn_static            VCUDA_defn_args(static, VCUDA_defn_type(
-#define VCUDA_defn_scope(rest)       VCUDA_defn_ ## rest))
-#define __global__                   VCUDA_defn_scope
-#define __global_init__              (void)gridDim; (void)blockDim;\
-                                     (void)blockIdx; (void)threadIdx;\
-                                     (void)kernelParams; (void)extra;}
-
-/*----------------------------------------------------------------------------*/
-/*! */
-/*----------------------------------------------------------------------------*/
-#define __syncthreads() _Pragma("omp barrier")
 
 /*----------------------------------------------------------------------------*/
 /*! */
@@ -86,7 +32,8 @@ typedef enum cudaError {
   CUDA_ERROR_NOT_INITIALIZED,
   CUDA_ERROR_INVALID_VALUE,
   CUDA_ERROR_LAUNCH_FAILED,
-  CUDA_ERROR_INVALID_MEMCPY_DIRECTION
+  CUDA_ERROR_INVALID_MEMCPY_DIRECTION,
+  CUDA_ERROR_OUT_OF_MEMORY
 } CUresult;
 
 /*----------------------------------------------------------------------------*/
@@ -100,7 +47,7 @@ enum cudaMemcpyKind {
 /*----------------------------------------------------------------------------*/
 /*! */
 /*----------------------------------------------------------------------------*/
-typedef int CUstream;
+typedef std::size_t CUstream;
 
 /*----------------------------------------------------------------------------*/
 /*! */
